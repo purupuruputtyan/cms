bedbfecef1158a5473947ec7a7e3df08f7980350
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-10 02:27:29 +0000

    [Add]BooksController記述追加

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -32,30 +32,28 @@ class BooksController extends Controller
         ]);
     }
 
-    // BooksController.php
-
     public function edit($book_id)
     {
         $book = Book::getBookByUserId($book_id, Auth::user()->id);
-    
+
         return view('books.edit', [
             'book' => $book
         ]);
     }
-    
+
     public function update(Request $request)
     {
         $book = Book::getBookByUserId($request->id, Auth::user()->id);
-    
+
         if ($book) {
             $validator = $book->validate($request->all());
-    
+
             if ($validator->fails()) {
                 return redirect('/')
                     ->withInput()
                     ->withErrors($validator);
             }
-    
+
             // 画像のアップロード処理
             if ($request->hasFile('item_img')) {
                 $file = $request->file('item_img');
@@ -63,7 +61,7 @@ class BooksController extends Controller
                 $file->move('upload', $filename);
                 $book->item_img = $filename;
             }
-    
+
             $book->fill($request->only([
                 'item_name',
                 'item_number',
@@ -72,8 +70,45 @@ class BooksController extends Controller
             ]));
             $book->save();
         }
-    
+
         return redirect('/');
     }
 
+    public function store(Request $request)
+    {
+        $validator = Book::validate($request->all());
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $file = $request->file('item_img');
+        $filename = '';
+
+        if (!empty($file)) {
+            $filename = $file->getClientOriginalName();
+            $file->move('../upload/', $filename);
+        } else {
+            $filename = 'no_image.jpg'; // デフォルトの画像ファイル名
+        }
+
+        $book = new Book;
+        $book->user_id = Auth::user()->id;
+        $book->item_name = $request->item_name;
+        $book->item_number = $request->item_number;
+        $book->item_amount = $request->item_amount;
+        $book->item_img = $filename;
+        $book->published = $request->published;
+        $book->save();
+
+        return redirect('/')->with('message', '本登録が完了しました');
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect('/');
+    }
 }
